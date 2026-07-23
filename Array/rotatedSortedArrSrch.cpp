4b1c9e42ff4cbf2a4cc619fdcac5f053e87f86d0
sprogram24-dotcom <sprogram24@gmail.com>
2026-07-23 13:56:45 +0530

    search in rotated sorted array-- first try

---
+++ Array/rotatedSortedArrSrch.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int binSearch(int arr[], int target,int start, int end){
+    int mid=start+(end-start)/2;
+    if (target>arr[mid]){
+        start=mid+1;
+    }else if(target<arr[mid]){
+        end-mid-1;
+    }else{
+        return mid;
+    }
+}
+
+int rotateSearch(int arr[],int s,int target){
+    int start=0;
+    int end=s-1;
+    int mid=start+(end-start)/2;
+    if(arr[start]>arr[mid]){
+        int y=binSearch(arr,target,mid+1,end);
+        if(target==arr[y]){
+            return y;
+        }else{
+            for(int i=start;i<mid;i++){
+                if(target==arr[i]){
+                return i;
+                }
+            }
+        }
+        
+    }else{
+        int x=binSearch(arr,target,start,mid-1);
+        if(target==arr[x]){
+            return x;
+        }else{
+            for(int i=mid+1;i<=end;i++){
+                if(target==arr[i]){
+                return i;
+                }
+            } 
+        }
+    }     
+}
+
+
+int main(){
+    int arr[]={3,4,5,6,7,0,1,2};
+    int arr1[]={6,7,0,1,2,3,4,5};
+    cout<<rotateSearch(arr1,8,0);
+    return 0;
+}
