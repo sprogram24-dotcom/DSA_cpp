8f8af9e40ef92c935eeb247f9edd145cadeec9fc
sprogram24-dotcom <sprogram24@gmail.com>
2026-07-24 20:01:26 +0530

    book allocation

---
+++ Array/bookallocRev.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+bool isvalid(int arr[],int n, int m, int maxAlloc){
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxAlloc) return false;
+    }
+    int stu=1;
+    int pg=0;
+    for(int i=0;i<n;i++){
+        if(pg+arr[i]<=maxAlloc){
+            pg+=arr[i];
+        }else{
+            stu++;
+            pg=arr[i];
+        }
+    }
+   if(stu>m){
+    return false;
+   }else{
+    return true;
+   }
+}
+
+int bookAlloc(int arr[],int n, int m){
+    if(m>n) return -1;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int start=0,end=sum;
+    int ans;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(isvalid(arr,n,m,mid)){
+            ans=mid;
+            end=mid-1;
+        }else{
+            start=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int arr[]={2,1,1,1};
+    cout<<bookAlloc(arr,4,2);
+    return 0;
+}
