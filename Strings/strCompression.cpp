7f2c66156b879678d4022fcddf0312185b7be6d7
sprogram24-dotcom <sprogram24@gmail.com>
2026-07-31 10:00:57 +0530

    string compression

--- Strings/strCompression.cpp
+++ Strings/strCompression.cpp
@@ -1,32 +1,66 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
 
-int strCom(char str[]){
-    int count=1;
-    int n=sizeof(str);
-    string s;
-    for(int i=0;i<n;i++){
-        if(str[i]==str[i+1]){
-            count++;
-        }else{
-            if(count==1){
-                s+=str[i];
-            }else{
-                s+=str[i]+to_string(count);
-            }
-            count=1;
+int strCom(vector<char> str){
+    int idx=0;
+    int n=str.size();
+    for(int i=0;i<str.size();i++){
+        char ch=str[i];
+        int count=0;
+        while(ch==str[i] && i<n){
+            count++; i++;
         }
+        if(count==1){
+            str[idx]=ch;
+            idx++;
+        }else{
+            str[idx]=ch;
+            idx++;
+            string ct=to_string(count);
+            for(char chr: ct){
+                str[idx]=chr;
+                idx++;
+            }
+        }
+        i--;
     }
-    return s.length();
+    return idx;
+}
+
+int revision(vector<char> s){
+    int idx=0;
+    int n=s.size();
+    for(int i=0;i<n;i++){
+        int count=0;
+        char ch=s[i];
+        while(i<n && s[i]==ch){
+            count++; i++;
+        }
+        if(count==1){
+            s[idx]=ch;
+            idx++;
+        }else{
+            s[idx]=ch;
+            idx++;
+            string str=to_string(count);
+            for(char dig: str){
+                s[idx]=dig;
+                idx++;
+            }
+        }
+        i--;
+    }
+    s.resize(idx);
+    return idx;
 }
 
 
 
 int main(){
     char str[]={'a','a','b','b','c','c','c'};
-    char str1[]={'a','b','b','b','b','b','b','b','b','b','b','b','b'};
-    cout<<strlen(str1);
-    cout<<strCom(str1);
+    vector<char> str1={'a','a','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','b','c','c','c'};
+    cout<<revision(str1);
     return 0;
 }
