1d607d78aa1923e3b2910344ef17376264d2329f
sprogram24-dotcom <sprogram24@gmail.com>
2026-07-31 13:24:15 +0530

    counting prime in range

--- Math/prime.cpp
+++ Math/prime.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 bool checkPrime(int num){
-    for(int i=0;i*i<=num;i++){
+    for(int i=2;i*i<=num;i++){
         if(num%i==0){
             return false;
         }
     }
     return true;
 }
+//TLE
+void prime_in_range(int st, int end){
+    int count=0;
+    for(int i=st;i<=end;i++){
+        bool prime=true;
+        for(int j=2;j*j<=i;j++){
+            if(i%j==0){
+                prime=false;
+            }
+        }
+        if(prime){
+            count++;
+            cout<< i<<" ";
+        }
+    }
+    cout<<endl<<count;
+}
+
+//sieve of eratosthenes
+int countPrime(int num){
+    vector<bool> isPrime(num,true);
+    int count=0;
+    for(int i=2;i<num;i++){
+        if(isPrime[i]){
+            count++;
+            
+            for(int j=i*2;j<num;j=j+i){
+                isPrime[j]=false;
+            }
+        }
+    }
+    return count;
+}
+
 
 int main(){
-    int num=23;
-    cout<<2;
-    cout<<checkPrime(num);
+    int num=10;
+    cout<<countPrime(num);
     return 0;
 }
